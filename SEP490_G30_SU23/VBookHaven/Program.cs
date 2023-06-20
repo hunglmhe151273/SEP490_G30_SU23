6d648cf2fd672fa895e4a9ce88f466c078e52004
hunglmhe151273 <101168208+hunglmhe151273@users.noreply.github.com>
2023-06-20 18:46:55 +0700

    Add login function

--- SEP490_G30_SU23/VBookHaven/Program.cs
+++ SEP490_G30_SU23/VBookHaven/Program.cs
@@ -1,12 +1,17 @@
 using VBookHaven.Models;
 using Microsoft.EntityFrameworkCore;
+using VBookHaven.Respository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession(opt => opt.IdleTimeout = TimeSpan.FromMinutes(60));
 builder.Services.AddDbContext<VBookHavenDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddTransient<IAccountRespository, AccountRespository>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,13 +20,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
-
+app.UseSession();
 app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
---
+++ SEP490_G30_SU23/VBookHaven/Respository/AccountRespository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VBookHaven.Models;
+
+namespace VBookHaven.Respository
+{
+    public interface IAccountRespository
+    {
+        Task<Models.Account> findByEmailAndPassword(string email, string password);
+    }
+    public class AccountRespository : IAccountRespository
+    {
+
+        private readonly VBookHavenDBContext _dbContext;
+        public AccountRespository(VBookHavenDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Models.Account> findByEmailAndPassword(String email, String password)
+        {
+                var acc = await _dbContext.Accounts.Where(u => u.Password.Equals(password) && u.Username.Equals(email)).FirstOrDefaultAsync();
+                return acc;
+        }
+    }
+
+
+}
