dee41cbb1c38ac3e58cd6bc6cd5c7e53749ad542
hunglmhe151273 <101168208+hunglmhe151273@users.noreply.github.com>
2023-07-15 23:50:57 +0700

    Ignore file json

--- SEP490_G30_SU23/VBookHaven.DataAccess/Migrations/20230715150533_initDB.Designer.cs
+++ SEP490_G30_SU23/VBookHaven.DataAccess/Migrations/20230715150533_initDB.Designer.cs
@@ -12,8 +12,8 @@ using VBookHaven.DataAccess.Data;
 namespace VBookHaven.DataAccess.Migrations
 {
     [DbContext(typeof(VBookHavenDBContext))]
-    [Migration("20230715133606_changeOrderShipInfo")]
-    partial class changeOrderShipInfo
+    [Migration("20230715150533_initDB")]
+    partial class initDB
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
