c8ac476db40009f4f2b5728fe64001134985c019
KhaChinh <101690105+KhaChinh@users.noreply.github.com>
2023-07-25 09:26:27 +0700

    Update FE Invoice
    - Validate input
    - add button "Lưu"

---
+++ SEP490_G30_SU23/VBookHaven/wwwroot/mgmt-lib/js/validate.js
@@ -0,0 +1,133 @@
+// Change style for invalid input
+function invalidInput(inputId, feedbackId, message) {
+    $(inputId).addClass("is-invalid");
+    $(inputId).removeClass("is-valid");
+    $(feedbackId).addClass("invalid-feedback");
+    $(feedbackId).removeClass("valid-feedback");
+    $(feedbackId).text(message);
+}
+
+// Change style for valid input
+function validInput(inputId, feedbackId) {
+    $(inputId).addClass("is-valid");
+    $(inputId).removeClass("is-invalid");
+    $(feedbackId).addClass("valid-feedback");
+    $(feedbackId).removeClass("invalid-feedback");
+    $(feedbackId).text("OK");
+}
+
+// Reset all input
+function resetForm(button) {
+    $(button).parent().parent().find('.feedback').each(function () {
+        $(this).text('');
+        $(this).removeClass("valid-feedback");
+        $(this).removeClass("invalid-feedback");
+    });
+    $(button).parent().parent().find('.important').each(function () {
+        $(this).removeClass("is-valid");
+        $(this).removeClass("is-invalid");
+    })
+}
+//-----------------------------------------------------------------------------------------------------------------
+
+//Add product
+$(document).on('keyup', '#productName', function () {
+    if ($(this).val().length >= 20 || $(this).val() == "") {
+        invalidInput(this, $('#product-feedback'), "Tên sản phẩm không được vượt quá 20 kí tự");
+    } else {
+        validInput(this, $('#product-feedback'))
+    }
+});
+
+$(document).on('keyup', '#barCode', function () {
+    if ($(this).val().length >= 100) {
+        invalidInput(this, $('#barCode-feedback'), "Tên sản phẩm không được vượt quá 100 kí tự");
+    } else {
+        validInput(this, $('#barCode-feedback'))
+    }
+});
+
+$(document).on('keyup', '#price', function () {
+    if (isNaN($(this).val()) || $(this).val() == 0) {
+        invalidInput(this, $('#price-feedback'), "Giá trị phải là số tự nhiên lớn hơn 0");
+    } else {
+        validInput(this, $('#price-feedback'))
+    }
+});
+
+$(document).on('keyup', '#quantity', function () {
+    if (isNaN($(this).val()) || $(this).val() == 0) {
+        invalidInput(this, $('#quantity-feedback'), "Giá trị phải là số tự nhiên lớn hơn 0");
+    } else {
+        validInput(this, $('#quantity-feedback'))
+    }
+});
+
+$(document).on('keyup', '#unit', function () {
+    if ($(this).val().length >= 100 || $(this).val() == "") {
+        invalidInput(this, $('#unit-feedback'), "Đơn vị không được vượt quá 100 kí tự");
+    } else {
+        validInput(this, $('#unit-feedback'))
+    }
+});
+//-----------------------------------------------------------------------------------------------------------------
+
+// Add supplier
+$(document).on('keyup', '#supplier', function () {
+    if ($(this).val().length >= 100) {
+        invalidInput(this, $('#supplier-feedback'), "Tên nhà cung cấp không được vượt quá 100 kí tự");
+    } else if ($(this).val() == "") {
+        invalidInput(this, $('#supplier-feedback'), "Tên nhà cung cấp là bắt buộc");
+    } else {
+        validInput(this, $('#supplier-feedback'))
+    }
+});
+
+$(document).on('keyup', '#phone', function () {
+    if ($(this).val().length != 10) {
+        invalidInput(this, $('#phone-feedback'), "Số điện thoại phải có đúng 10 kí tự");
+    } else if ($(this).val() == "") {
+        invalidInput(this, $('#phone-feedback'), "Số điện thoại cấp là bắt buộc");
+    } else if (isNaN($(this).val())) {
+        invalidInput(this, $('#phone-feedback'), "Số điện thoại chỉ được chứa chữ số");
+    } else {
+        validInput(this, $('#phone-feedback'))
+    }
+});
+
+$(document).on('keyup', '#description', function () {
+    if ($(this).val().length >= 1000) {
+        invalidInput(this, $('#description-feedback'), "Ghi chú không được vượt quá 1000 kí tự");
+    } else {
+        validInput(this, $('#description-feedback'))
+    }
+});
+//-----------------------------------------------------------------------------------------------------------------
+
+// Reset form
+$(document).on('click', '.reset', function () {
+    resetForm(this);
+});
+
+// Submit button enabled when all input valid
+$(document).on('keyup', '.addForm', function () {
+    var input = 0;
+    var valid = 0;
+    $(this).find('.validate').each(function () {
+        input++;
+    })
+    $(this).find('.is-valid').each(function () {
+        valid++;
+    })
+    if (input == valid) {
+        $(this).find('.submit').removeAttr('disabled');
+    } else {
+        $(this).find('.submit').attr('disabled','disabled');
+    }
+})
+
+// Set submit button disable
+$(document).ready(function () {
+    $('#submitProduct').attr('disabled','disabled');
+    $('#submitSupplier').attr('disabled','disabled');
+})
