ba25158550cd7f1c93f4efcbcd3f74a93f2775ec
KhaChinh <101690105+KhaChinh@users.noreply.github.com>
2023-08-22 01:20:36 +0700

    Unit Test

---
+++ SEP490_G30_SU23/VBookHaven.Test/AuthorControllerTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using VBookHaven.Models;
+using VBookHaven_Admin.Areas.Admin.Controllers;
+
+namespace VBookHaven.Test
+{
+    [TestFixture]
+    public class AuthorControllerTest
+    {
+        private TransactionScope scope;
+
+        [SetUp]
+        public void Setup()
+        {
+            scope = new TransactionScope();
+        }
+
+        [TearDown]
+        public void Test1()
+        {
+            scope.Dispose();
+        }
+
+        [Test]
+        public void GetAllAuthors_Returns_IActionResult()
+        {
+            var authorController = new AuthorController();
+            var data = authorController.Index();
+            Assert.IsNotNull(data);
+        }
+
+        [Test]
+        public async Task AddAuthor_Returns_OkResult()
+        {
+            var authorController = new AuthorController();
+            Author aTest = new Author
+            {
+                AuthorName = "Test",
+                Description = "Good author",
+                Status = true
+            };
+            var data = authorController.Add(aTest);
+            var result = await data;
+            Assert.IsInstanceOf<ObjectResult>(result);
+        }
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(1000)]
+        public async Task ChangeAuthorStatus_Returns_404(int authorId)
+        {
+            var authorController = new AuthorController();
+            var data = authorController.ChangeStatus(authorId);
+            var result = await data;
+            Assert.IsInstanceOf<ObjectResult>(result);
+        }
+    }
+}
---
+++ SEP490_G30_SU23/VBookHaven.Test/ProductControolerTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using VBookHaven.Models;
+using VBookHaven_Admin.Areas.Admin.Controllers;
+
+namespace VBookHaven.Test
+{
+    [TestFixture]
+    public class ProductControolerTest
+    {
+        private TransactionScope scope;
+
+        [SetUp]
+        public void Setup()
+        {
+            scope = new TransactionScope();
+        }
+
+        [TearDown]
+        public void Test1()
+        {
+            scope.Dispose();
+        }
+
+        [Test]
+        public void GetAllProduct_Returns_IActionResult()
+        {
+            var productController = new ProductController();
+            var data = productController.Index();
+            Assert.IsNotNull(data);
+        }
+
+        [Test]
+        public void AddBook_Return_IActionResult()
+        {
+            var productController = new ProductController();
+            Product pTest = new Product
+            {
+                Name = "Book1",
+                Barcode = "0123012",
+                Unit = "Chiec",
+                UnitInStock = 0,
+                PurchasePrice = 12000,
+                RetailPrice = 15000,
+                RetailDiscount = 0.2,
+                WholesalePrice = 13000,
+                WholesaleDiscount = 0.1,
+                Size = "20x30x30",
+                Weight = "1kg",
+                Description = "Good Book",
+                Status = true,
+                IsBook = true,
+                SubCategoryId = 1
+            };
+            Book bTest = new Book
+            {
+                Pages = 340,
+                Language = "VietNam",
+                Publisher = "Hung Le",
+            };
+            ProductManagementViewModel modelTest = new ProductManagementViewModel
+            {
+                Product = pTest,
+                Book = bTest,
+                Stationery = null,
+            };
+            var data = productController.AddBook(modelTest);
+            Assert.IsTrue(data != null);
+        }
+
+        [Test]
+        public void AddStationery_Return_IActionResult()
+        {
+            var productController = new ProductController();
+            Product pTest = new Product
+            {
+                Name = "Stationery1",
+                Barcode = "0125552",
+                Unit = "Hop",
+                UnitInStock = 0,
+                PurchasePrice = 10000,
+                RetailPrice = 12000,
+                RetailDiscount = 0.2,
+                WholesalePrice = 11000,
+                WholesaleDiscount = 0.1,
+                Size = "2X14",
+                Weight = "80g",
+                Description = "Good Stationery",
+                Status = true,
+                IsBook = false,
+                SubCategoryId = 2
+            };
+            Stationery sTest = new Stationery
+            {
+                Material = "Nhua",
+                Origin = "Viet Nam",
+                Brand = "Thien Long"
+            };
+            ProductManagementViewModel modelTest = new ProductManagementViewModel
+            {
+                Product = pTest,
+                Book = null,
+                Stationery = sTest,
+            };
+            var data = productController.AddStationery(modelTest);
+            Assert.IsTrue(data != null);
+        }
+    }
+}
